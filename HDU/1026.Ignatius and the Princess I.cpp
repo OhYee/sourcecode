9cef966761b7c8ddb19e144d97d23fb951c697f1
孔晨皓 <oyohyee@oyohyee.com>
2016-04-19 20:59:59 +0800

    push

--- HDU/1026.Ignatius and the Princess I.cpp
+++ HDU/1026.Ignatius and the Princess I.cpp
@@ -8,6 +8,7 @@ Blog:http://www.cnblogs.com/ohyee/
 エリーチカ！
 要写出来Хорошо的代码哦~
 */
+
 #include <cstdio>
 #include <algorithm>
 #include <cstring>
@@ -18,6 +19,7 @@ Blog:http://www.cnblogs.com/ohyee/
 #include <list>
 #include <queue>
 #include <stack>
+#include <map>
 using namespace std;
 
 //DEBUG MODE
@@ -27,209 +29,149 @@ using namespace std;
 #define REP(n) for(int o=0;o<n;o++)
 
 
+const int maxn = 155;
 
-const int maxn = 10;
-int n, m;
-int Map[maxn][maxn];
-
-
-const int delta[4] = { 1,-1,0,0 };
+int N,M;
+char Map[maxn][maxn];
+pair<int,int> last[maxn][maxn];
+int dis[maxn][maxn];
 
 struct point {
-	int x, y;
-	int time;
+	int x,y;
 	int dis;
-	point(int a, int b, int c, int d) {
+	point(int a,int b,int c) {
 		x = a;
 		y = b;
-		time = c;
-		dis = d;
+		dis = c;
+	}
+	bool operator < (const point &rhs)const {
+		return dis > rhs.dis;
 	}
 };
 
-int BFS(int s1, int s2, int v1, int v2) {
-	queue<point> Q;
-	int mark[maxn][maxn];
-	memset(mark, -1, sizeof(mark));
-	int ans = -1;
 
-	Q.push(point(s1, s2, 6, 0));
-	mark[s1][s2] = 6;
+const int delta[] = {1,-1,0,0};
+
+int BFS() {
+	priority_queue<point> Q;
+	bool visited[maxn][maxn];
+	memset(visited,false,sizeof(visited));
+	memset(dis,-1,sizeof(dis));
+
+	Q.push(point(0,0,0));
+	dis[0][0] = 0;
+
 	while (!Q.empty()) {
-		point temp = Q.front();
+
+		int th1 = Q.top().x;
+		int th2 = Q.top().y;
+		int thdis = Q.top().dis;
 		Q.pop();
 
-		int x = temp.x;
-		int y = temp.y;
-		int time = temp.time;
-		int dis = temp.dis;
-
-		if (x == v1 && y == v2) {
-			ans = ((ans == -1) ? (dis) : min(ans, dis));
-			//printf("	(%d,%d)%d %d\n", x,y,time,dis);
-		}
-
-		//**
-		REP(4) {
-			int xx = x + delta[o];
-			int yy = y + delta[3-o];
-			int tt = time - 1;
-			int dd = dis + 1;
-
-			if (xx < 0 || xx >= n || yy < 0 || yy >= m
-				|| Map[xx][yy] == 0 || tt == 0)
-				continue;
-			if (Map[xx][yy] == 4)
-				tt = 6;
-			if (mark[xx][yy] < tt) {
-				mark[xx][yy] = tt;
-				//printf("%d %d %d %d\n", xx, yy, tt, dd);
-				Q.push(point(xx, yy, tt, dd));
-			}
-		}
-	}
-	return ans;
-}
-
-void Do() {
-	char s1, v1;
-	int s2, v2;
-	scanf("%d%d\n", &n, &m);
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++) {
-			scanf("%d", &Map[i][j]);
-			if (Map[i][j] == 2) {
-				s1 = i;
-				s2 = j;
-				Map[i][j] = 1;
-			}
-			if (Map[i][j] == 3) {
-				v1 = i;
-				v2 = j;
-				Map[i][j] = 1;
-			}
-		}
-	printf("%d\n", BFS(s1, s2, v1, v2));
-}
-
-
-int main() {
-	int T;
-	scanf("%d", &T);
-	while (T--)
-		Do();
-	return 0;
-}
-
-/*
-
-#include <cstdio>
-#include <algorithm>
-#include <cstring>
-#include <cmath>
-#include <string>
-#include <iostream>
-#include <vector>
-#include <list>
-#include <queue>
-#include <stack>
-using namespace std;
-
-//DEBUG MODE
-#define debug 0
-
-//循环
-#define REP(n) for(int o=0;o<n;o++)
-
-typedef pair<int, int> point;
-
-const int maxn = 10;
-int n, m;
-int Map[maxn][maxn];
-
-
-const int delta[4] = { 1,-1,0,0 };
-
-int BFS(int s1, int s2, int v1, int v2) {
-	queue<pair<int, point> > Q;
-	bool visited[maxn][maxn][maxn];
-	memset(visited, false, sizeof(visited));
-	int dis[maxn][maxn][maxn];
-	memset(dis, -1, sizeof(dis));
-
-	Q.push(pair<int, point>(6, point(s1, s2)));
-	visited[6][s1][s2] = true;
-	dis[6][s1][s2] = 0;
-	while (!Q.empty()) {
-		int time = Q.front().first;
-		int th1 = Q.front().second.first;
-		int th2 = Q.front().second.second;
-		Q.pop();
+		if (visited[th1][th2] == true)
+			continue;
+		visited[th1][th2] = true;
 
 		//达到终点
-		if (th1 == v1 && th2 == v2)
-			break;
+		//if (th1 == N - 1 && th2 == M - 1)
+		//	return dis[N - 1][M - 1];
 
 		//拓展节点
-		int next1, next2, nt;
-		for (int i = 0; i < 4; i++) {
-			nt = time-1;
-			next1 = th1 + delta[i];
-			next2 = th2 + delta[3 - i];
-			
-			if (next1 >= n || next1 < 0 || next2 >= m || next2 < 0
-				|| Map[next1][next2] == 0 || nt == 0)
-				continue;
+		REP(4) {
+			int next1 = th1 + delta[o];
+			int next2 = th2 + delta[3 - o];
 
-			if (!visited[nt][next1][next2]) {
-				if (Map[next1][next2] == 4)
-					nt = 6;
+			if (Map[next1][next2] != 'X' && next1 >= 0
+				&& next1 < N && next2 >= 0 && next2 < M) {
 
-				Q.push(pair<int, point>(nt, point(next1, next2)));
-				visited[nt][next1][next2] = true;
-				dis[nt][next1][next2] =
-					dis[nt][next1][next2] == -1 ?
-					dis[time][th1][th2] + 1 :
-					min(dis[time][th1][th2] + 1, dis[nt][next1][next2]);
+				int weight;
+				if (Map[next1][next2] == '.')
+					weight = 1;
+				else
+					weight = Map[next1][next2] - '0' + 1;
+
+				int temp = dis[next1][next2];
+
+				dis[next1][next2] = dis[next1][next2] == -1 ?
+					dis[th1][th2] + weight :
+					min(dis[th1][th2] + weight,dis[next1][next2]);
+
+				if (temp != dis[next1][next2]) {//记录路径
+					last[next1][next2] = pair<int,int>(th1,th2);
+					//printf("%d %d -> %d %d\n", th1, th2, next1, next2);
+				}
+
+				Q.push(point(next1,next2,dis[next1][next2]));
+
 			}
+
 		}
+
 	}
-	
-	int ans = dis[0][v1][v2];
-	for (int i = 0; i < 6; i++)
-		if (dis[i][v1][v2] != -1)
-			ans =ans==-1?dis[i][v1][v2]: min(ans, dis[i][v1][v2]);
-
-	return ans;
+	if (dis[N - 1][M - 1])
+		return dis[N - 1][M - 1];
+	else
+		return -1;
 }
 
-void Do() {
-	char s1, v1;
-	int s2, v2;
-	scanf("%d%d\n", &n, &m);
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++) {
-			scanf("%d", &Map[i][j]);
-			if (Map[i][j] == 2) {
-				s1 = i;
-				s2 = j;
-				Map[i][j] = 1;
-			}
-			if (Map[i][j] == 3) {
-				v1 = i;
-				v2 = j;
-				Map[i][j] = 1;
-			}
+bool Do() {
+	if (scanf("%d%d",&N,&M) == EOF)
+		return false;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			scanf("\n%c\n",&Map[i][j]);
+	/*
+	for (int i = 0; i < N; i++) {
+	for (int j = 0; j < M; j++)
+	printf("%c", Map[i][j]);
+	printf("\n");
+	}
+	*/
+
+	if (BFS() == -1) {
+		printf("God please help our poor hero.\nFINISH\n");
+	} else {
+		printf("It takes %d seconds to reach the target position, let me show you the way.\n",dis[N - 1][M - 1] );
+
+		stack<pair<int,int> > s;
+		int x = N - 1,y = M - 1;
+		while (!(x == 0 && y == 0)) {
+			//printf("s %d %d\n", x, y);
+			s.push(pair<int,int>(x,y));
+			pair<int,int> t = last[x][y];
+			x = t.first;
+			y = t.second;
 		}
-	printf("%d\n", BFS(s1, s2, v1, v2));
+		//s.pop();
+		//s.push(point(0, 0));
+
+		REP(dis[N - 1][M - 1]) {
+			printf("%ds:",o + 1);
+
+			x = s.top().first;
+			y = s.top().second;
+			s.pop();
+			printf("(%d,%d)->(%d,%d)\n",last[x][y].first,last[x][y].second,x,y);
+
+			if (Map[x][y] >= '0'&&Map[x][y] <= '9') {
+				int wait = Map[x][y] - '0';
+				for (o++; wait; wait--,o++)
+					printf("%ds:FIGHT AT (%d,%d)\n",o + 1,x,y);
+				o--;
+			}
+
+
+		}
+		printf("FINISH\n");
+
+	}
+
+
+
+	return true;
 }
 
-
-int vs_main() {
-	int T;
-	scanf("%d", &T);
-	while (T--)
-		Do();
+int main() {
+	while (Do());
 	return 0;
 }
-*/
