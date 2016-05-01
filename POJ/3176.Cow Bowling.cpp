633512b52a321cf2fd3dc29918e6d5a758846246
OhYee <oyohyee@oyohyee.com>
2016-05-02 00:49:02 +0800

    1

---
+++ POJ/3176.Cow Bowling.cpp
@@ -0,0 +1,68 @@
+/*
+By:OhYee
+Github:OhYee
+HomePage:http://www.oyohyee.com
+Email:oyohyee@oyohyee.com
+Blog:http://www.cnblogs.com/ohyee/
+
+かしこいかわいい？
+エリーチカ！
+要写出来Хорошо的代码哦~
+*/
+
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+using namespace std;
+
+//DEBUG MODE
+#define debug 0
+
+//循环
+#define REP(n) for(int o=0;o<n;o++)
+
+#define t(n) (((n) * ((n)+1))/2)
+
+const int maxn = 1005;
+int n;
+int a[t(maxn)];
+int dp[t(maxn)];
+
+int left(int n) {
+	int i;
+	for(i = 0;t(i) < n;i++);
+	return n + i;
+}
+
+bool Do() {
+	if(scanf("%d",&n) == EOF)
+		return false;
+	for(int i = 1;i <= t(n);i++)
+		scanf("%d",&a[i]);
+
+	memset(dp,0,sizeof(dp));
+	for(int i = 0;i <= t(n - 1);i++) {
+		dp[left(i)] = max(dp[left(i)],dp[i] + a[left(i)]);
+		dp[left(i) + 1] = max(dp[left(i) + 1],dp[i] + a[left(i) + 1]);
+	}
+
+	int Max = -1;
+	for(int i = t(n - 1) + 1;i <= t(n);i++)
+		Max = max(Max,dp[i]);
+
+	printf("%d\n",Max);
+	return true;
+}
+
+int main() {
+	while(Do());
+	return 0;
+}
--- t.cpp
+++ t.cpp
@@ -1,50 +1,123 @@
-#include <iostream>  
-#include <cstdio>
-using namespace std;  
-  
-int min(int a,int b)  
-{  
-    return a<b?a:b;  
-}  
-  
-int main()  
-{  
-	int i;
-	int j;
-	freopen("in.txt","r",stdin);
-    int test;  
-    cin>>test;  
-    while(test--)  
-    {  
-        /*Input & Initial*/  
-  
-        int c;  
-        cin>>c;  
-  
-        int* a=new int[c+1];  //某类珍珠数目  
-        int* p=new int[c+1];  //某类珍珠单价  
-        int* dp=new int[c+1]; //dp[i]表示在已知第i类珍珠时,所需支付的最低价格  
-        int* sum=new int[c+1];//sum[i]=∑a[i]  
-  
-        sum[0]=0;  
-        for(i=1;i<=c;i++)  
-        {  
-            cin>>a[i]>>p[i];  
-            sum[i]=sum[i-1]+a[i];  
-        }  
-  
-        /*Dp*/  
-  
-        dp[0]=0;  //Dp初始化  
-        for(i=1;i<=c;i++)  
-        {  
-            dp[i]=(a[i]+10)*p[i]+dp[i-1];   //当第i种珍珠出现时，未优化价格的情况  
-            for(j=0;j<i;j++)  //枚举第i种珍珠前的每一种珍珠，寻找最优价格  
-                dp[i]=min(dp[i],dp[j]+(sum[i]-sum[j]+10)*p[i]);  //在求dp[i]前，对于每一个j<i，dp[j]的最优值已求出  
-        }                                                        //(sum[i]-sum[j]+10)*p[i]即第j+1~i种珍珠被第i种珍珠替代后的价格  
-        cout<<dp[c]<<endl;  
-  
-        delete a,p,dp,sum;  
-    }  
-    return 0;  
-}  
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct node
+{
+   int member;
+   struct node * pNext;
+}Node,*pNode;
+
+typedef struct stack
+{
+   pNode Top;
+   pNode Bottom;
+}Stack,*pStack;
+
+int InitStack(pStack );
+int Push(pStack,int);
+void TraverseStack(pStack);
+int Empty(pStack);
+int Pop(pStack);
+void Clear(pStack);
+
+int main(void)
+{
+   Stack s;
+   int i;
+   int num;
+   int data;
+   int re_num;
+   InitStack(&s);
+   printf("璇疯緭鍏ヨ繘鏍堝厓绱犱釜鏁:\n");
+   scanf("%d",&num);
+    for(i=0;i<num;i++)
+    {
+        printf("绗%d涓暟锛",i+1);
+        scanf("%d",&data);
+        if (Push(&s,data))
+        {
+           continue;
+        }
+        else
+        {
+            printf("鎿嶄綔澶辫触\n");
+            exit(-1);
+        }
+    }
+    for(i=0;i<num;i++)
+    {
+      re_num=Pop(&s);
+      printf("杈撳嚭鏍堝唴绗%d鍏冪礌涓猴細%d\n",i+1,re_num);}
+      printf("璇ユ爤鐨勯暱搴︿负%d\n",num);
+       if (Empty(&s) == 0)
+         printf("鏍堜负绌篭n");
+       else
+         printf("鏍堜笉涓虹┖\n");
+
+}
+int InitStack(pStack ps)
+{
+    ps->Top = (pNode)malloc(sizeof(Node));
+    if (NULL == ps->Top)
+    {
+        printf("鍔ㄦ佸垎閰嶅唴瀛樺け璐n");
+        exit(-1);
+    }
+    else
+    {
+        ps->Bottom = ps->Top;
+        ps->Top->pNext = NULL;
+    }
+    return 0;
+}
+
+int Push(pStack ps,int data)
+{
+     pNode pNew=(pNode)malloc(sizeof(Node));
+     if(NULL == pNew)
+     {
+         return 0;
+     }
+    pNew->member = data;
+    pNew->pNext = ps -> Top;
+    ps->Top = pNew;
+
+    return 1;
+    }
+
+void TraversesStack(pStack ps)
+{
+    pNode pNew = ps -> Top;
+    while(pNew!=ps->Bottom)
+    {
+        printf("%d",pNew->member);
+        pNew = pNew ->pNext;
+    }
+  return ;
+  }
+int Empty(pStack ps)
+  {
+    if(ps->Top == ps->Bottom)
+        return 0;
+  else
+        return 1;
+    }
+
+  int Pop(pStack ps)
+  {
+      pNode pSwap =NULL;
+      int return_val;
+      if(Empty(ps))
+      {
+          exit(-1);
+      }
+  else
+{
+    return_val = ps->Top->member;
+    pSwap =ps->Top;
+    ps->Top = ps->Top->pNext;
+    free(pSwap);
+    return return_val;
+    }
+  }
