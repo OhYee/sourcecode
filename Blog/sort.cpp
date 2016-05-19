120a97af9e3566feed59c3b4c0e82776e96ff48e
孔晨皓 <oyohyee@oyohyee.com>
2016-05-19 23:17:17 +0800

    增加了堆排序，发现了快排的bug

--- Blog/sort.cpp
+++ Blog/sort.cpp
@@ -1,150 +1,216 @@
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-#include <memory>
+#include "stdafx.h"
 
-#define random(x) (rand()%x)
-#define REP(n) for(int o=0;o<n;o++)
+#include <cstdio> 
+ #include <cstdlib> 
+ #include <ctime> 
+ #include <memory> 
+  
+ #define random(x) (rand()%x) 
+ #define REP(n) for(int o=0;o<n;o++) 
+  
+ const bool SHOW=0; 
+ const int size=1000; 
+ const int MAX=100000; 
+ const int TIME=100; 
+  
+ int a[size]; 
+ int b[size]; 
+ int ans[size];
+  
+ long long start; 
+  
+ //冒泡排序 
+ void BubbleSort(int a[],int n) { 
+ 	int ThisSwapPosition = n;//本轮最后一个交换的位置 
+ 	bool HasSwap = false; 
+ 	for(int i = 0;i < n;i++) { 
+ 		HasSwap = false; 
+ 		int LastSwapPosition = ThisSwapPosition;//上轮最后一个交换的位置 
+ 		for(int j = 1;j < LastSwapPosition;j++) { 
+ 			if(a[j - 1] > a[j]) { 
+ 				int temp = a[j - 1]; 
+ 				a[j - 1] = a[j]; 
+ 				a[j] = temp; 
+ 				ThisSwapPosition = j; 
+ 				HasSwap = true;//本轮发生交换 
+ 			} 
+ 		} 
+ 		if(!HasSwap) 
+ 			break; 
+ 	} 
+ } 
+  
+ //归并排序 
+ void Merge(int a[],int l,int mid,int r) { 
+ 	int pos1 = l;//左侧的指针 
+ 	int pos2 = mid + 1;//右侧的指针 
+ 	int *temp = new int[r - l + 1]; 
+ 	int pos = 0;//临时数组的指针 
+ 	while(pos1 <= mid || pos2 <= r) { 
+ 		if(pos1 > mid) { 
+ 			temp[pos++] = a[pos2++]; 
+ 		} 
+ 		if(pos2 > r) { 
+ 			temp[pos++] = a[pos1++]; 
+ 		} 
+ 		if(pos1 <= mid&&pos2 <= r) { 
+ 			if(a[pos1] <= a[pos2]) { 
+ 				temp[pos++] = a[pos1++]; 
+ 			} else { 
+ 				temp[pos++] = a[pos2++]; 
+ 			} 
+ 		} 
+ 	} 
+ 	for(int i = 0;i <= r - l;i++) 
+ 		a[l + i] = temp[i]; 
+ } 
+  
+ //对a[l]~a[r]排序 
+ void MergeSort(int a[],int l,int r) { 
+ 	if(l<r) { 
+ 		int mid = (l + r) / 2; 
+ 		MergeSort(a,l,mid); 
+ 		MergeSort(a,mid + 1,r); 
+ 		Merge(a,l,mid,r); 
+ 	} 
+ } 
+  
+  
+ //快速排序 
+ void QuickSort(int a[],int l,int r){ 
+ 	if(l>=r) 
+ 		return ; 
+	//TODO:修复当仅剩余两个逆序数时，不交换的问题
+	//if(r-l==1){
+	//	if(a[l]>a[r]){
+	//		int temp=a[l];
+	//		a[l]=a[r];
+	//		a[r]=temp;
+	//	}
+	//	return;
+	//}
+ 	int left=l; 
+ 	int right=r; 
+ 	int mid=(a[l]+a[r]+a[(l+r)/2])/3; 
+ 	while(left < right){ 
+ 		while(a[right] >= mid && left < right) 
+ 			right--; 
+ 		while(a[left] <= mid && left < right) 
+ 			left++; 
+ 		int temp=a[right]; 
+ 		a[right]=a[left]; 
+ 		a[left]=temp; 
+  
+ 	} 
+ 	QuickSort(a,l,left); 
+ 	QuickSort(a,right+1,r); 
+ } 
+  
 
-const bool SHOW=1;
-const int size=10;
-const int MAX=1000;
-const int TIME=10;
-
-int a[size];
-int b[size];
-
-long long start;
-
-//冒泡排序
-void BubbleSort(int a[],int n) {
-	int ThisSwapPosition = n;//本轮最后一个交换的位置
-	bool HasSwap = false;
-	for(int i = 0;i < n;i++) {
-		HasSwap = false;
-		int LastSwapPosition = ThisSwapPosition;//上轮最后一个交换的位置
-		for(int j = 1;j < LastSwapPosition;j++) {
-			if(a[j - 1] > a[j]) {
-				int temp = a[j - 1];
-				a[j - 1] = a[j];
-				a[j] = temp;
-				ThisSwapPosition = j;
-				HasSwap = true;//本轮发生交换
-			}
-		}
-		if(!HasSwap)
-			break;
+//调整堆
+ void AdjustHeap(int a[],int i,int n){
+	int left = (i+1)*2-1;
+	int right = (i+1)*2;
+	if(left<n && a[left]>a[i]){
+		int temp=a[left];
+		a[left]=a[i];
+		a[i]=temp;
+		AdjustHeap(a,left,n);
 	}
-}
-
-//归并排序
-void Merge(int a[],int l,int mid,int r) {
-	int pos1 = l;//左侧的指针
-	int pos2 = mid + 1;//右侧的指针
-	int *temp = new int[r - l + 1];
-	int pos = 0;//临时数组的指针
-	while(pos1 <= mid || pos2 <= r) {
-		if(pos1 > mid) {
-			temp[pos++] = a[pos2++];
-		}
-		if(pos2 > r) {
-			temp[pos++] = a[pos1++];
-		}
-		if(pos1 <= mid&&pos2 <= r) {
-			if(a[pos1] <= a[pos2]) {
-				temp[pos++] = a[pos1++];
-			} else {
-				temp[pos++] = a[pos2++];
-			}
-		}
-	}
-	for(int i = 0;i <= r - l;i++)
-		a[l + i] = temp[i];
-}
-
-//对a[l]~a[r]排序
-void MergeSort(int a[],int l,int r) {
-	if(l<r) {
-		int mid = (l + r) / 2;
-		MergeSort(a,l,mid);
-		MergeSort(a,mid + 1,r);
-		Merge(a,l,mid,r);
-	}
-}
-
-
-//快速排序
-void QuickSort(int a[],int l,int r){
-	if(l>=r)
-		return ;
-	int left=l;
-	int right=r;
-	int mid=(a[l]+a[r]+a[(l+r)/2])/3;
-	while(left < right){
-		while(a[right] > mid && left < right)
-			right--;
-		while(a[left] < mid && left < right)
-			left++;
+	if(right<n && a[right]>a[i]){
 		int temp=a[right];
-		a[right]=a[left];
-		a[left]=temp;
-
+		a[right]=a[i];
+		a[i]=temp;
+		AdjustHeap(a,right,n);
 	}
-	QuickSort(a,l,left);
-	QuickSort(a,right+1,r);
-}
-
-void Do(){
-	//生成随机数
-	REP(size)
-		a[o]=random(MAX);
-	//输出随机数
-	printf("生成随机数	：	");
-	if(SHOW)
-	REP(size)
-		printf("%d ",a[o]);
-	printf("\n");
 	
-	//测试1 - 冒泡排序
-	printf("冒泡排序");
-	memcpy(b,a,sizeof(a));
-	start=clock();
-	BubbleSort(b,size);
-	printf("%.4fs	:	",double(clock()-start)/ CLOCKS_PER_SEC);
-	if(SHOW)
-	REP(size)
-		printf("%d ",b[o]);
-	printf("\n");
+ }
 
-	//测试2 - 归并排序
-	printf("归并排序");
-	memcpy(b,a,sizeof(a));
-	start=clock();
-	MergeSort(b,0,size-1);
-	printf("%.4fs	:	",double(clock()-start)/ CLOCKS_PER_SEC);
-	if(SHOW)
-	REP(size)
-		printf("%d ",b[o]);
-	printf("\n");
+//堆排序
+ void HeapSort(int a[],int n){
+	 for(int i=n/2;i>=0;i--){
+		AdjustHeap(a,i,n);
+	 }
 
-	//测试3 - 快速排序
-	printf("快速排序");
-	memcpy(b,a,sizeof(a));
-	start=clock();
-	QuickSort(b,0,size-1);
-	printf("%.4fs	:	",double(clock()-start)/ CLOCKS_PER_SEC);
-	if(SHOW)
-	REP(size)
-		printf("%d ",b[o]);
-	printf("\n");
+	 for(int i=0;i<n-1;i++){
+		int temp=a[0];
+		a[0]=a[n-1-i];
+		a[n-1-i]=temp;
+		AdjustHeap(a,0,n-i-1);
+	 }
+ }
 
-	printf("\n");
 
-}
 
-int main(){
-	srand((int)time(0));
-	REP(TIME)
-		Do();
-    return 0;
-}
+ void Do(){ 
+ 	//生成随机数 
+ 	REP(size) 
+ 		a[o]=random(MAX); 
+ 	//输出随机数 
+ 	printf("生成随机数	：	"); 
+ 	if(SHOW) 
+ 	REP(size) 
+ 		printf("%d ",a[o]); 
+ 	printf("\n"); 
+ 	 
+ 	//测试1 - 冒泡排序 
+ 	printf("冒泡排序"); 
+ 	memcpy(b,a,sizeof(a)); 
+ 	start=clock(); 
+ 	BubbleSort(b,size); 
+ 	printf("%.4fs	:	",double(clock()-start)/ CLOCKS_PER_SEC); 
+	if(SHOW) 
+ 	REP(size) 
+ 		printf("%d ",b[o]); 
+	memcpy(b,ans,sizeof(b));
+ 	printf("\n"); 
+  
+ 	//测试2 - 归并排序 
+ 	printf("归并排序"); 
+ 	memcpy(b,a,sizeof(a)); 
+ 	start=clock(); 
+ 	MergeSort(b,0,size-1); 
+ 	printf("%.4fs	:	",double(clock()-start)/ CLOCKS_PER_SEC); 
+ 	if(SHOW) 
+ 	REP(size) 
+ 		printf("%d ",b[o]); 
+	if(!memcmp(ans,b,sizeof(b)))
+		printf("Wrong");
+ 	printf("\n"); 
+  
+ 	//测试3 - 快速排序 
+ 	printf("快速排序"); 
+ 	memcpy(b,a,sizeof(a)); 
+ 	start=clock(); 
+ 	QuickSort(b,0,size-1); 
+ 	printf("%.4fs	:	",double(clock()-start)/ CLOCKS_PER_SEC); 
+ 	if(SHOW) 
+ 	REP(size) 
+ 		printf("%d ",b[o]); 
+	if(!memcmp(ans,b,sizeof(b)))
+		printf("Wrong");
+ 	printf("\n"); 
+  
+	//测试4 - 堆排序 
+ 	printf("堆排序"); 
+ 	memcpy(b,a,sizeof(a)); 
+ 	start=clock(); 
+ 	HeapSort(b,size); 
+ 	printf("%.4fs	:	",double(clock()-start)/ CLOCKS_PER_SEC); 
+ 	if(SHOW) 
+ 	REP(size) 
+ 		printf("%d ",b[o]); 
+	if(!memcmp(ans,b,sizeof(b)))
+		printf("Wrong");
+ 	printf("\n"); 
+
+ 	printf("\n"); 
+  
+ } 
+  
+ int main(){ 
+ 	srand((int)time(0)); 
+ 	REP(TIME) 
+ 		Do(); 
+     return 0; 
+ } 
