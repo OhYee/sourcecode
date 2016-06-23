424efbc16983dbdce46ba5e817d58914d82e5240
OhYee <oyohyee@oyohyee.com>
2016-06-23 22:52:34 +0800

    poj 3349

---
+++ POJ/3349.Snowflake Snow Snowflakes.cpp
@@ -0,0 +1,115 @@
+/*
+By:OhYee
+Github:OhYee
+Blog:http://www.oyohyee.com/
+Email:oyohyee@oyohyee.com
+
+かしこいかわいい？
+エリーチカ！
+要写出来Хорошо的代码哦~
+*/
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+using namespace std;
+
+
+const int maxl = 10000005;
+const int maxn = 100005;
+const int HashSize = maxn;
+
+
+int HashTable[HashSize];
+int Next[maxn];
+int Link[maxn][6];
+int pos;
+
+void Init() {
+	pos = 0;
+	memset(Next,-1,sizeof(Next));
+	memset(HashTable,-1,sizeof(HashTable));
+}
+
+//hash
+bool HashCmp(int a[6],int b[6]) {
+	for(int i = 0;i < 6;i++) {
+		if(a[i] == b[0]) {
+			for(int k = 1;k == 1 || k == -1;k -= 2) {
+				for(int j = 1;j < 6;j++) {
+					int n = (i + k*j + 6) % 6;
+					if(a[n] != b[j])
+						break;
+					else
+						if(j == 5)
+							return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+int Hash(int a[6]) {
+	int sum = 0;
+	for(int i = 0;i < 6;i++)
+		sum += a[i];
+	return sum%HashSize;
+}
+
+bool HashInsert(int a[6]) {
+	int h = Hash(a);
+	if(HashTable[h] == -1) {
+		memcpy(Link[pos],a,6 * sizeof(int));
+		HashTable[h] = pos++;
+	} else {
+		int k = HashTable[h];
+		int last = HashTable[h];
+		while(k != -1) {
+			if(HashCmp(a,Link[k]))
+				return false;
+			last = k;
+			k = Next[k];
+		}
+		memcpy(Link[pos],a,6 * sizeof(int));
+		Next[last] = pos++;
+	}
+	return true;
+}
+
+bool Do() {
+	int n;
+	scanf("%d",&n);
+	Init();
+	bool Find = false;
+	int a[6];
+	for(int o = 0;o < n;o++) {
+		for(int i = 0;i < 6;i++)
+			scanf("%d",&a[i]);
+		if(!HashInsert(a)) {
+			Find = true;
+			break;
+		}
+	}
+
+	if(Find)
+		printf("Twin snowflakes found.\n");
+	else
+		printf("No two snowflakes are alike.\n");
+
+	return false;
+}
+
+int main() {
+	while(Do());
+	return 0;
+}
--- t.cpp
+++ t.cpp
@@ -1,20 +1,90 @@
-#include "stdio.h"
-int main() {
-  int i;
-  for(i = 0;;i++) {
-    //printf("%d\n",i);
-    int m = i * 10 + 6;
-    int bit = 1;
-    int temp = i;
-    while(temp) {
-      temp /= 10;
-      bit*=10;
-    }
-    int n = 6 * bit + i;
-    if(n == 4 * m) {
-      printf("%d\n",n);
-      break;
-    }
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <ctime>
+using namespace std;
+ 
+#define REP(n) for(int o=0;o<n;o++)
+ 
+pair<int,int> p[205];
+const int maxn =205;
+
+int cob(int n)  {
+  if (n == 3) return 1;
+  else {
+    int i = n, a = n, b = n - 3;
+    while (i >= 2) a *= --i;
+    i = n - 3;
+    while (i >= 2) b *= --i;
+    return a / (6 * b);
   }
-  return 0;
 }
+
+bool Do() {
+    int n;
+
+    int count = 0, i = 0;
+    signed int x[maxn], y[maxn];
+
+    n = rand()%100;
+    REP(n) {
+        int tx,ty;
+        x[o]=tx = rand()%100;
+        y[o]=ty = rand()%100;
+        p[o] = pair<int,int>(tx,ty);
+    }
+    printf("in\n");
+    int cnt = 0;
+    for(int i = 0;i < n;i++)
+        for(int j = i + 1;j < n;j++)
+            for(int k = j + 1;k < n;k++) {
+                if(i < j&&j < k) {
+                    pair<int,int >a = p[i],b = p[j],c = p[k];
+                    if(!(
+                        (a.first == b.first&&a.first == c.first) ||
+                        (a.second == b.second&&a.second == c.second) ||
+                        ((double)(a.first - b.first) / (double)(a.second - b.second) ==
+                            (double)(c.first - b.first) / (double)(c.second - b.second))
+                        )) {
+                        cnt++;
+                        //printf("%d %d %d\n",i,j,k);
+                    }
+                }
+            }
+printf("a\n");
+            int ans=0;
+    if (n == 1 || n == 2) ans=0;
+    else {
+    int j = 0, k = 0;
+    for (i = 0; i < n - 1; i++)
+    for (j = i + 1; j < n - 1; j++) {
+    for (k = j + 1; k < n; k++) {
+    if (((x[j] - x[i]) == 0) || ((x[k] - x[j] == 0))) {
+    if (((x[j] - x[i]) == 0) && ((x[k] - x[j] == 0))) {
+    count++; 
+    continue;
+    }
+    }
+    else if ((y[j] - y[i]) / (x[j] - x[i]) == (y[k] - y[j]) / (x[k] - x[j])) count++;
+    }
+    }
+    ans =cob(n)-count;
+    }
+
+    printf("%d %d\n",cnt,ans);
+ 
+    return true;
+}
+ 
+int main() {
+  srand((int)time(0));
+    while(Do());
+    return 0;
+}
+
