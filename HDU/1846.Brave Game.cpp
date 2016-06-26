34ba572b5fdb404414826ba224bbb89e355d3f7a
孔晨皓 <oyohyee@oyohyee.com>
2016-06-26 21:34:23 +0800

    hdu1846

---
+++ HDU/1846.Brave Game.cpp
@@ -0,0 +1,44 @@
+/*
+By:OhYee
+Github:OhYee
+Blog:http://www.oyohyee.com/
+Email:oyohyee@oyohyee.com
+
+かしこいかわいい？
+エリーチカ！
+要写出来Хорошо的代码哦~
+*/
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+using namespace std;
+
+
+
+void Do() {
+	int n,m;
+	scanf("%d%d",&n,&m);
+	if(n % (m + 1) != 0)
+		printf("first\n");
+	else
+		printf("second\n");
+}
+
+int vs_main() {
+	int T;
+	scanf("%d",&T);
+	while(T--) {
+		Do();
+	}
+	return 0;
+}
