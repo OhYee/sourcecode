c84877645f3cf6aaf4eac32b5e148f80744ef18e
孔晨皓 <oyohyee@oyohyee.com>
2016-07-31 15:40:07 +0800

    103

--- AOJ/103.BUY LOW, BUY LOWER.cpp
+++ AOJ/103.BUY LOW, BUY LOWER.cpp
@@ -3,7 +3,6 @@ By:OhYee
 Github:OhYee
 HomePage:http://www.oyohyee.com
 Email:oyohyee@oyohyee.com
-Blog:http://www.cnblogs.com/ohyee/
 
 かしこいかわいい？
 エリーチカ！
@@ -24,19 +23,11 @@ Blog:http://www.cnblogs.com/ohyee/
 #include <set>
 using namespace std;
 
-//DEBUG MODE
-#define debug 0
-
-//循环
-#define REP(n) for(int o=0;o<n;o++)
-
 const int maxn = 5005;
 
 int a[maxn];
 int dp[maxn];
-bool vis[maxn];
 int cnt[maxn];
-int last[maxn];
 
 bool Do() {
 	int n;
@@ -50,43 +41,38 @@ bool Do() {
 
 	memset(dp,0,sizeof(dp));
 	memset(cnt,0,sizeof(cnt));
+	cnt[0] = 1;
 
-	int Maxpos = 0;
-	int pos=0;
+	int Max = 0;
 	for(int i = 1;i <= n;i++) {
-
-		for(int j = 0;j < i;j++)
+		for(int j = i - 1;j >= 0;j--)
 			if(a[j] > a[i] || j == 0)
-				if(dp[i] <= dp[j] + 1) {
-					if(dp[i] == dp[j] + 1) {
-						last[++last[0]] = j;
-					} else {
-						last[0] = 0;
-						dp[i] = dp[j] + 1;
-					}
-				}
+				dp[i] = max(dp[i],dp[j] + 1);
+		Max = max(dp[i],Max);
+	}
 
-		for(int j = 1;j <= i;j++)
-			vis[j] = false;
-
-		for(int j = 1;j <= last[last[0]];j++) {
-			if(!vis[j]) {
-				vis[j] = true;
+	for(int i = 1;i <= n;i++) {
+		for(int j = i - 1;j >= 0;j--) {
+			if(a[i] == a[j])
+				break;
+			if(dp[i] == dp[j] + 1 && (a[i] <= a[j] || j == 0)) {
 				cnt[i] += cnt[j];
 			}
 		}
-
-		if(dp[pos] < dp[i])
-			pos = i;
 	}
 
+	int num = 0;
+	for(int i = 1;i <= n;i++) {
+		if(Max == dp[i])
+			num += cnt[i];
+	}
 
-	printf("%d %d\n",dp[pos],cnt[pos]);
+	printf("%d %d\n",Max,num);
 
 	return true;
-}
 
-int vs_main() {
+}
+int main() {
 	while(Do());
 	return 0;
 }
