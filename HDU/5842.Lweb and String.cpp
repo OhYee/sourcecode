ad6a3b6d391c3a231e43d8f03bdbd03aa8cc5f60
孔晨皓 <oyohyee@oyohyee.com>
2016-08-15 10:43:32 +0800

    5842

---
+++ HDU/5842.Lweb and String.cpp
@@ -0,0 +1,60 @@
+/*
+By:OhYee
+Github:OhYee
+Blog:http://www.oyohyee.com/
+Email:oyohyee@oyohyee.com
+
+かしこいかわいい？
+エリーチカ！
+要写出来Хорошо的代码哦~
+*/
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <bitset>
+#include <iomanip> 
+using namespace std;
+
+const int maxn = 27;
+bool vis[maxn];
+
+void Do() {
+	char c = getchar();;
+
+	memset(vis,false,sizeof(vis));
+	int ans = 0;
+	while(!(c >= 'a'&&c <= 'z'))
+		c=getchar();
+	while(c >= 'a'&&c <= 'z') {
+		int t = c - 'a';
+		if(!vis[t]) {
+			ans++;
+			vis[t] = true;
+		}
+
+		c = getchar();
+	}
+	printf("%d\n",ans);
+}
+
+int main() {
+	//cin.tie(0);
+	//cin.sync_with_stdio(false);
+	int T;
+	scanf("%d",&T);
+	for(int i = 1;i <= T;i++) {
+		printf("Case #%d: ",i);
+		Do();
+	}
+	return 0;
+}
