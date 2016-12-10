38ea038b9f01fb9649605b924f4ff5f8d256ea0b
孔晨皓 <oyohyee@oyohyee.com>
2016-12-10 15:07:13 +0800

    VScode

--- Temp/Temp.cpp
+++ Temp/Temp.cpp
@@ -1,16 +1,9 @@
 #include <cstdio>
-#include <iostream>
-#include <iomanip>
 using namespace std;
-const double PI = 3.1415927;
 int main(){
-	/*
-	double r;
-	while(scanf("%lf",&r)!=EOF)
-		printf("%.3f\n",4.0/3.0*PI*r*r*r);
-	*/
-	double r;
-	while(cin>>r)
-		cout<<fixed<<setprecision(3)<<4.0/3.0*PI*r*r*r<<endl;
+	int a;
+	printf("Hello World!");
+	a= 1000;
+	printf("%d",a);
 	return 0;
 }
