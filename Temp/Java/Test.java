15d85cc81c9bf17513effa216dbbf62128650150
OhYee <oyohyee@oyohyee.com>
2017-06-24 12:06:06 +0800

    learn java

---
+++ Temp/Java/Test.java
@@ -0,0 +1,11 @@
+public class Test {  
+    public static void main (String []args)  
+    {  
+        System.out.println("90 度的正弦值：" + Math.sin(Math.PI/2));  
+        System.out.println("0度的余弦值：" + Math.cos(0));  
+        System.out.println("60度的正切值：" + Math.tan(Math.PI/3));  
+        System.out.println("1的反正切值： " + Math.atan(1));  
+        System.out.println("π/2的角度值：" + Math.toDegrees(Math.PI/2));  
+        System.out.println(Math.PI);  
+    }
+}
