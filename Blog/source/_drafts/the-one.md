2ac7bcce7b07679ad0498bea307a57c9a71c67ac
OhYee <oyohyee@oyohyee.com>
2017-07-11 09:52:46 +0800

    日常同步

---
+++ Blog/source/_drafts/the-one.md
@@ -0,0 +1,10 @@
+---
+title: The-One 仿真软件修改
+date: 2017-07-08 00:39:12
+categories: 笔记
+tags:
+  - Java
+  - the-one
+  - DTNs
+---
+The-ONE
--- Temp/test.py
+++ Temp/test.py
@@ -1,9 +1,8 @@
-#coding = utf-8
-f = open('problem.txt','r',encoding='gbk')
-while True:
-    line = f.readline()
-    if len(line) == 0: 
-        break
-    else:
-        print(line)
-f.close()
+import numpy as np
+import pylab as pl
+ 
+x = [1, 2, 3, 4, 5]     # Make an array of x values
+y = [1, 4, 9, 16, 25]   # Make an array of y values for each x value
+ 
+pl.plot(x, y)           # use pylab to plot x and y
+pl.show()               # show the plot on the screen
