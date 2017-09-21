4a52631bc9855b2a5c335631d5733d43a68a5e81
OhYee <oyohyee@oyohyee.com>
2017-09-21 23:08:30 +0800

    hdu 6206

--- ACM/HDU/6206.Apple.java
+++ ACM/HDU/6206.Apple.java
@@ -62,15 +62,15 @@ public class Main {
         return new Segment(mp, pa(mp, vec));
     }
 
-    static Point Seg_Seg(Segment L1, Segment L2) {
+    static Point Seg_Seg(Segment L1, Segment L2, BigDecimal[] f) {
         BigDecimal a = L1.b.x.subtract(L1.a.x);
         BigDecimal b = L2.b.x.subtract(L2.a.x);
         BigDecimal c = L1.b.y.subtract(L1.a.y);
         BigDecimal d = L2.b.y.subtract(L2.a.y);
-        BigDecimal f = a.multiply(d).subtract(b.multiply(c));
+        f[0] = a.multiply(d).subtract(b.multiply(c));
         BigDecimal g = L2.b.x.subtract(L1.a.x);
         BigDecimal h = L2.b.y.subtract(L1.a.y);
-        BigDecimal t = d.multiply(g).subtract(b.multiply(h)).divide(f);
+        BigDecimal t = d.multiply(g).subtract(b.multiply(h));
         Point p = new Point(L1.a.x.add(t.multiply(a)), L1.a.y.add(t.multiply(c)));
         return p;
     }
@@ -96,18 +96,6 @@ public class Main {
             System.out.print("\n");
     }
 
-    public static Point waixin(Point a, Point b, Point c) {
-        BigDecimal a1 = b.x.subtract(a.x);
-        BigDecimal b1 = b.y.subtract(a.y);
-        BigDecimal c1 = (a1.multiply(a1).add(b1.multiply(b1))).divide(two);
-        BigDecimal a2 = c.x.subtract(a.x);
-        BigDecimal b2 = c.y.subtract(a.y);
-        BigDecimal c2 = (a2.multiply(a2).add(b2.multiply(b2))).divide(two);
-        BigDecimal d = a1.multiply(b2).subtract(a2.multiply(b1));
-        return new Point(a.x.add(c1.multiply(b2).subtract(c2.multiply(b1))).divide(d),
-                a.y.add(a1.multiply(c2).subtract(a2.multiply(c1))).divide(d));
-    }
-
     public static void main(String[] args) {
         cin = new Scanner(new BufferedInputStream(System.in));
         two = BigDecimal.valueOf(2);
@@ -120,30 +108,25 @@ public class Main {
             for (int i = 0; i < 4; ++i) {
                 p[i] = read_point();
             }
-            //Segment a = getSegment(new Segment(p[0], p[1]));
-            //Segment b = getSegment(new Segment(p[1], p[2]));
+            Segment a = getSegment(new Segment(p[0], p[1]));
+            Segment b = getSegment(new Segment(p[1], p[2]));
 
-            //Point o = Seg_Seg(a, b);
+            BigDecimal[] f = { two };
+            Point o = Seg_Seg(a, b, f);
 
-            Point o1 = waixin(p[0], p[1], p[2]);
-            Point o2 = waixin(p[0], p[2], p[1]);
-            BigDecimal r = distance(p[0], o1);
-            Point o;
+            p[0].x = p[0].x.multiply(f[0]);
+            p[0].y = p[0].y.multiply(f[0]);
+            p[3].x = p[3].x.multiply(f[0]);
+            p[3].y = p[3].y.multiply(f[0]);
 
-            if (r.subtract(distance(p[1], o1)).signum() == 0 && r.subtract(distance(p[2], o1)).signum() == 0) {
-                o = o1;
-            } else {
-                r = distance(p[0], o2);
-                o = o2;
-            }
-
-            BigDecimal dis = distance(o, p[3]);
+            BigDecimal r = distance(p[0], o);
+            BigDecimal dis = distance(p[3], o);
 
             //sprint(a);
             //sprint(b);
-            System.out.println(r);
-            pprint(o, true);
-            System.out.println(dis);
+            // System.out.println(r);
+            // pprint(o, true);
+            // System.out.println(dis);
 
             if (dis.subtract(r).signum() > 0) {
                 System.out.println("Accepted");
